259ffeab4171dcc81cd258aef15674a832870645
Filipe Arantes Fernandes <filran@gmail.com>
2023-04-15 19:25:49 -0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+var http = require('http');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.send('Please, select a Virtual World and enjoy!');
+});
+
+router.get('/:id', (req, res, next)=>{
+  const API = `http://localhost:3030/virtualworlds/${req.params.id}`;
+
+  //Get VW data (tip from https://dev.to/isalevine/three-ways-to-retrieve-json-from-the-web-using-node-js-3c88)
+  http.get(API, response =>{
+    let vwdata = {};
+
+    response.on('data', (vw)=>{
+      vwdata = JSON.parse(vw);
+    });
+
+    response.on('end', ()=>{
+      if(vwdata.id != undefined ){
+        res.render('index', { title: 'MetaSEE', id: vwdata.id});
+      }else{
+        res.redirect('/');
+      }
+    });
+
+  }).on('error', error => {
+    console.log(error.message);
+    res.redirect('/');
+  });
+});
+
+module.exports = router;
---
+++ src/main.js
@@ -0,0 +1,5 @@
+require('aframe');
+require('aframe-extras');
+require('aframe-event-set-component');
+require('aframe-environment-component');
+require('networked-aframe');
