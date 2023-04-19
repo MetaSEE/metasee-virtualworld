a609171d8df4f289918fd80da2013ffe777bb5ab
Filipe Arantes Fernandes <filran@gmail.com>
2023-04-19 12:23:35 -0300

    log removed

--- public/javascripts/metasee-extensions/virtualuml/metasee.virtualuml.extension.js
+++ public/javascripts/metasee-extensions/virtualuml/metasee.virtualuml.extension.js
@@ -108,8 +108,6 @@ AFRAME.registerComponent('a-umlclass-component', {
     // Do something when component's data is updated.
     this.self.setClassName();
     this.self.changeClassWidthAccordingClassName();
-
-    console.log('changed');
   },
 
   remove: function () {
